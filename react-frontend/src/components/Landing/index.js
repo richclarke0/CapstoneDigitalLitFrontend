6b23d4c6eb933ac9d81a2dbfa3cca3c87ae803a7
Richard Clarke <freshnewrich@gmail.com>
2022-08-13 18:11:52 -0700

    stuff

--- react-frontend/src/components/Landing/index.js
+++ react-frontend/src/components/Landing/index.js
@@ -1,6 +1,14 @@
 
 import React from 'react';
-const App = () => ( <div>
+const App = () => ( 
+<div>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
     <h1>App</h1>
   </div>
 );
--- react-frontend/src/components/PasswordForget/index.js
+++ react-frontend/src/components/PasswordForget/index.js
@@ -2,6 +2,10 @@
 import React from 'react';
 const App = () => ( <div>
     <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
+    <h1>App</h1>
   </div>
 );
 export default App;
