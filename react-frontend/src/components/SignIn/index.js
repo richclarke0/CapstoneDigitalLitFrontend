786b57bedba70b8aa3f0918824f068403be27408
Richard Clarke <freshnewrich@gmail.com>
2022-08-14 21:13:52 -0700

    cleanup signin, begin signup

--- react-frontend/src/components/SignIn/index.js
+++ react-frontend/src/components/SignIn/index.js
@@ -1,53 +1,62 @@
 import { useEffect, useState } from "react"
 import React from 'react';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import axios from "axios"
-import {Button} from "react-foundation"
+import { Button } from "react-foundation"
 
 const App = (props) => {
 
   const [loginState, setLoginState] = useState({
-    email: '',
+    email: 'freshnewrich+0@gmail.com',
     password: '',
+    errors: [],
+    loading: false
   })
 
-  const [errorState, setErrorState] = useState(null)
 
   const handleChange = (event) => {
     setLoginState((prevState) => ({
       ...prevState,
       [event.target.name]: event.target.value,
-  }))
+    }))
   }
   const navigate = useNavigate()
-  
+
   const handleSubmit = (event) => {
-		event.preventDefault();
-		axios
-			.post('/login', loginState, )
-			.then((response) => {
-        console.log("res/token", response.data)
-				localStorage.setItem('AuthToken', `Bearer ${response.data.token}`);
-
+    event.preventDefault();
+    setLoginState((prevState) => ({
+      ...prevState,
+      loading: true,
+    }))
+    axios
+      .post('/login', loginState,)
+      .then((response) => {
+        // console.log("res/token", response.data)
+        localStorage.setItem('AuthToken', `Bearer ${response.data.token}`);
+        setLoginState((prevState) => ({
+          ...prevState,
+          loading: false,
+        }))
         navigate('/')
-			})
-			.catch((error) => {				
-				setErrorState({
-					errors: error.response.data,
-					loading: false
-				});
-			});
-	};
+      })
+      .catch((error) => {
+        setLoginState((prevState) => ({
+          ...prevState,
+          errors: error.response.data,
+          loading: false
+        }))
+      });
+  };
 
 
-  const {email,password} = loginState;
+  const { email, password } = loginState;
 
   return (
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" value={email} onChange={handleChange}/><br />
-        <input type="password" name="password" value={password} onChange={handleChange}/><br />
+        <input type="email" name="email" value={email} onChange={handleChange} /><br />
+        <input type="password" name="password" value={password} onChange={handleChange} /><br />
         <Button type="submit" value="submit">Submit</Button>
       </form>
     </div>
--- react-frontend/src/components/SignUp/index.js
+++ react-frontend/src/components/SignUp/index.js
@@ -1,7 +1,30 @@
+import axios from "axios"
+import {useEffect, useState} from "react"
+import { useNavigate } from "react-router-dom"
 
 import React from 'react';
-const App = () => ( <div>
-    <h1>App</h1>
-  </div>
-);
+const App = (props) => {
+  const [signupState, setSignupState] = useState(
+    {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      country: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  )
+
+  // const handleChange = (event) => {
+  //   setLoginState((prevState) => ({
+  //     ...prevState,
+  //     [event.target.name]: event.target.value,
+  //   }))
+  // }
+
+  const navigate = useNavigate()
+
+};
 export default App;
